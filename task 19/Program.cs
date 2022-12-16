ee7e885dcc559baf0026e018b65803c04f7a50f4
Gleb Mazhara <gensores@mail.ru>
2022-12-16 22:31:07 +0300

    Add tasks

---
+++ task 19/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("введите 5 значное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n%10;
+int b = (n/10)%10;
+int n1 = n/1000;
+int n2 = a*10+b;
+if(n1==n2)
+    Console.WriteLine($"число полиндром: {n}");
+else
+ Console.WriteLine($"число не полиндром: {n}");
---
+++ task 21/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Введите кординаты точек");
+Console.WriteLine("Введите X1");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Y1");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Z1");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите X2");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Y2");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Z2");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+Console.WriteLine($"Расстояние между двумя точками: {result}");
